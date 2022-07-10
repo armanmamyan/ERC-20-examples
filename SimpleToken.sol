c1e1c845a1c31684d2c6c6addea1185131c5ae4e
Arman <armmamyan@gmail.com>
2022-07-10 20:16:20 +0400

    Create SimpleToken.sol

---
+++ SimpleToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    
+    constructor() ERC20("My Token", "MTN") {
+        _mint(msg.sender, 100000 * 10 ** 18);
+        
+    }
+
+    modifier onlyAuthorized() {
+        require(owner() == msg.sender);
+        _;
+    }
+
+    function mint(address to, uint amount) external onlyAuthorized {
+        _mint(to, amount);
+    }
+
+    function burn(uint amount) external {
+        _burn(msg.sender, amount);
+    }
+}
